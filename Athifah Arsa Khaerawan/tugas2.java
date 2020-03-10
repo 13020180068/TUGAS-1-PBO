1155e2bf03e2fd492779a776b0dfd68583b18a56
athifaarsa <62011053+athifaarsa@users.noreply.github.com>
2020-03-10 21:08:36 +0800

    Add files via upload

---
+++ Athifah Arsa Khaerawan/tugas2.java
@@ -0,0 +1,11 @@
+/*
+	Nama 	: Athifah Arsa Khaerawan
+	Tanggal	: 10 Maret 2020
+	Waktu	: 19:00
+*/
+public class tugas2{
+	public static void main(String[] args)
+	{
+		System.out.print ("Belajar Bahasa Pemrograman Java");
+	}
+}
